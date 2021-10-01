167ff926b722c4c514c0fa3b7f9016b8bae7857e
Habeeb David <habeebdh1@gmail.com>
2021-10-01 07:25:15 +0100

    UI temporary done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express'),
   app = express();
 
+const PORT = process.env.PORT;
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
@@ -8,8 +10,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log('=============================');
-  console.log('Server Started at port' + ' ' + process.env.PORT);
+  console.log(`Server Started at port ${PORT}`);
   console.log('=============================');
 });
